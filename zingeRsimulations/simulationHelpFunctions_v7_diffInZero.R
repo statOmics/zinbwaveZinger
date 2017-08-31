e65ca649128c7b11e544a20807e545bcebdc28b0
koenvandenberge <koen.vdberge@gmail.com>
2017-08-31 11:28:51 +0200

    islam_sims_fc2 to new simulation, suppress warnings in GAMLSS ZTNB fit

--- zingeRsimulations/simulationHelpFunctions_v7_diffInZero.R
+++ zingeRsimulations/simulationHelpFunctions_v7_diffInZero.R
@@ -13,7 +13,7 @@ getParamsZTNB <- function(counts, offset, design=NULL) {
     if(length(countsModel)<2) stop("Need at least two positive counts")
     libSizeModel = libSize[counts>0]
     if(is.null(design)){designFit=matrix(1,nrow=length(countsModel),ncol=1)} else {designFit=design[counts>0,]}
-    fit=try(gamlss(formula=countsModel~-1+designFit+offset(log(libSizeModel)), family="NBIZeroTruncated", control=gamlss.control(trace=FALSE, n.cyc=300)),silent=TRUE)
+    fit=suppressWarnings(try(gamlss(formula=countsModel~-1+designFit+offset(log(libSizeModel)), family="NBIZeroTruncated", control=gamlss.control(trace=FALSE, n.cyc=300)),silent=TRUE))
 
     if(class(fit)[1]=="try-error") return(c(dispersion=NA, lambda=NA))
     lambda=exp(mean(fit$mu.coefficients)) #geometric mean
