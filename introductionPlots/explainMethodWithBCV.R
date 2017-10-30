3b8b559951bed4db8b615c4f065cf7171823e43f
koenvandenberge <koen.vdberge@gmail.com>
2017-10-30 10:24:49 +0100

    update code for Figure 2

--- introductionPlots/explainMethodWithBCV.R
+++ introductionPlots/explainMethodWithBCV.R
@@ -19,7 +19,6 @@ computeZinbwaveWeights <- function(zinb, counts){
 ######## RNA-seq prep #############
 ###################################
 #### no zero inflation simulation
-library(Biobase)
 load("../datasets/bottomly_eset.RData")
 bottomly=exprs(bottomly.eset)
 bottomly=bottomly[!rowSums(bottomly)==0,]
@@ -32,6 +31,7 @@ DEind = sample(1:nTags,floor(nTags/20),replace=FALSE) #5% differentially express
 fcSim <- (2 + rexp(length(DEind), rate = 1)) #adapted from Soneson et al. 2016, Genome Biology
 set.seed(1)
 dataNoZI <- NBsim(foldDiff = fcSim, ind=DEind, dataset = bottomly, nTags = nTags, group = grp, verbose = TRUE, add.outlier = FALSE, drop.extreme.dispersion = FALSE, lib.size=libSize, drop.low.lambda=TRUE)
+
 
 data=dataNoZI$counts
 d=DGEList(data)
@@ -109,12 +109,65 @@ cellType[stemCellID] <- "stemCell"
 islam = as.matrix(countData)
 islam = islam[!rowSums(islam>0)<5,]
 
-## BCV for islam
-design=model.matrix(~cellType)
-dIslam=DGEList(islam)
+
+### simulate data
+paramsIslam = getDatasetMoMPositive(counts = islam)
+nSamples=80
+grp=as.factor(rep(0:1, each = nSamples/2)) #two-group comparison
+nTags=10000 #nr of features
+set.seed(11)
+DEind = sample(1:nTags,floor(nTags*.1),replace=FALSE) #10% DE
+fcSim=(2 + rexp(length(DEind), rate = 1/2)) #fold changes
+libSizes=sample(colSums(islam),nSamples,replace=TRUE) #library sizes
+simDataIslam <- NBsimSingleCell(foldDiff = fcSim, ind = DEind,
+                                dataset = islam, nTags = nTags,
+                                group = grp,
+                                verbose = TRUE, params = paramsIslam,
+                                lib.size = libSizes, normalizeLambda=TRUE)
+
+### make BCV from simulated data
+design=model.matrix(~grp)
+dIslam=DGEList(simDataIslam$counts)
 dIslam=calcNormFactors(dIslam)
 dIslam=estimateGLMTagwiseDisp(estimateGLMCommonDisp(dIslam,design, interval=c(0,10)),design,prior.df=0)
 dIslamTrend=estimateDisp(dIslam,design,prior.df=0)
+
+## get ZINB-WaVE weights
+islamEset = core <- SummarizedExperiment(simDataIslam$counts, colData = data.frame(group = grp))
+zinbModel = zinbFit(islamEset, X="~group", epsilon=1e12)
+zeroWeights = computeZinbwaveWeights(zinbModel, simDataIslam$counts)
+## calculate dispersion
+dW=DGEList(simDataIslam$counts)
+dW=calcNormFactors(dW)
+dW$weights=zeroWeights
+dW=estimateDisp(dW,design,prior.df=0)
+
+## ROC simulated data zero excess
+pvalSeq = c(1e-15,1e-14,1e-13,1e-12,1e-10,1e-9,1e-8,1e-7,1e-6,seq(.00001,.005,by=.00001),seq(.005,1,by=.005))
+falses=which(simDataIslam$dropout==1)
+samp=which(simDataIslam$dropout==0)
+tprSc=fprSc=vector(length=length(pvalSeq))
+for(i in 1:length(pvalSeq)){
+   excessID <- which(zeroWeights<=pvalSeq[i])
+   tprSc[i] <- mean(samp%in%excessID)
+   fprSc[i] <- mean(falses%in%excessID)
+}
+plot(x=fprSc,y=tprSc,type="l", xlab="False positive rate", ylab="True positive rate", lwd=2, col="steelblue")
+
+
+
+## BCV after weighting
+
+
+
+
+
+## BCV for islam
+# design=model.matrix(~cellType)
+# dIslam=DGEList(islam)
+# dIslam=calcNormFactors(dIslam)
+# dIslam=estimateGLMTagwiseDisp(estimateGLMCommonDisp(dIslam,design, interval=c(0,10)),design,prior.df=0)
+# dIslamTrend=estimateDisp(dIslam,design,prior.df=0)
 
 ## get ZINB-WaVE weights
 ##estimate ZINB model
@@ -126,6 +179,17 @@ dW=DGEList(islam)
 dW=calcNormFactors(dW)
 dW$weights=zeroWeights
 dW=estimateDisp(dW,design,prior.df=0)
+
+## ROC
+pvalSeq = c(1e-15,1e-14,1e-13,1e-12,1e-10,1e-9,1e-8,1e-7,1e-6,seq(.00001,.005,by=.00001),seq(.005,1,by=.005))
+falses=which(dataNoZI$counts==0)
+tpr=fpr=vector(length=length(pvalSeq))
+for(i in 1:length(pvalSeq)){
+   excessID <- which(zeroWeightsSim<=pvalSeq[i])
+   tpr[i] <- mean(samp%in%excessID)
+   fpr[i] <- mean(falses%in%excessID)
+}
+plot(x=fpr,y=tpr,type="l", xlab="False positive rate", ylab="True positive rate", lwd=2, col="steelblue")
 
 
 #######################################
@@ -139,15 +203,15 @@ library(Hmisc)
 cuts=cut2(wMean,cuts=c(0,0.25,0.75,1))
 
 
-png("~/Dropbox/phdKoen/singleCell/zinbwaveZingerGithub/zinbwaveZinger/introductionPlots/introBCV_v2.png", width=10,height=5, units="in", res=330)
+png("~/Dropbox/phdKoen/singleCell/zinbwaveZingerGithub/zinbwaveZinger/introductionPlots/introBCV_v3.png", width=10,height=5, units="in", res=330)
 #pdf("~/Dropbox/phdKoen/singleCell/zinbwaveZingerGithub/zinbwaveZinger/introductionPlots/introBCV_v2.pdf", width=10,height=5)
 #dev.new(width=10,height=5)
 ##### plot for paper: RNA-seq
 library(scales)
-par(mar=c(4.1,4.25,3,1),bty="l", mfrow=c(2,4), cex.lab=1.5, cex.axis=1.5)
+par(mar=c(4.1,4.25,3,1),bty="l", mfrow=c(2,3), cex.lab=1.5, cex.axis=1.5)
 
 cols=c("red","gold","black")
-plot(x=dZeroes$AveLogCPM,y=sqrt(dZeroes$tagwise.dispersion),pch=16,cex=.2,ylim=c(0,2.5),xlab="Average Log CPM", ylab="BCV",col=alpha(cols[as.numeric(cuts)],1/3), type="n")
+plot(x=dZeroes$AveLogCPM,y=sqrt(dZeroes$tagwise.dispersion),pch=16,cex=.2,ylim=c(0,2.5),xlab="Average log CPM", ylab="BCV",col=alpha(cols[as.numeric(cuts)],1/3), type="n")
 #sapply(c(4,1,2,3),function(i){
 sapply(c(3,1,2),function(i){
 	points(x=dZeroes$AveLogCPM[cuts==levels(cuts)[i]],y=sqrt(dZeroes$tagwise.dispersion)[cuts==levels(cuts)[i]],pch=16,cex=.2, col=alpha(cols[i]))
@@ -160,42 +224,35 @@ mtext("a" ,at=-5, font=2, cex=4/3)
 
 ## ROC
 plot(x=c(0,fpr),y=c(0,tpr),type="l", xlab="False positive rate", ylab="True positive rate", lwd=2, col="steelblue")
-legend("bottomright", "ZINB-WaVE weights", lty=1, col="steelblue", bty="n")
+legend("bottomright", "Bottomly ZINB-WaVE weights", lty=1, col="steelblue", bty="n")
 mtext("b" ,at=-0.22, font=2, cex=4/3)
 
 
-hist(zeroId[zeroId==0],xlim=c(0,.95),breaks=seq(0,1,.05),main="",xlab="Posterior probability")
-hist(zeroWeightsSim[samp],add=TRUE,breaks=seq(0,1,.05),col=rgb(0.1,0.8,0.1,.2))
-legend("topright",c("nr. true excess zeros","ZINB-WaVE prob."),fill=c(0,rgb(0.1,0.8,0.1,.2)), bty="n", cex=1.25)
-mtext("c" ,at=-0.22, font=2, cex=4/3)
-
-
-plot(x=dWeighted$AveLogCPM,y=sqrt(dWeighted$tagwise.dispersion),pch=16,cex=.2,ylim=c(0,2.5),xlab="Average Log CPM", ylab="BCV",col=alpha("black",1/3))
+plot(x=dWeighted$AveLogCPM,y=sqrt(dWeighted$tagwise.dispersion),pch=16,cex=.2,ylim=c(0,2.5),xlab="Average log CPM", ylab="BCV",col=alpha("black",1/3))
 o <- order(dWeighted$AveLogCPM)
 lines(dOriginal$AveLogCPM[o], sqrt(dOriginalTrend$trended.dispersion)[o], col = "red",lwd = 2)
 lines(dZeroes$AveLogCPM[o], sqrt(dZeroesTrend$trended.dispersion)[o], col = "blue",lwd = 2)
 lines(dWeighted$AveLogCPM[o], sqrt(dWeightedTrend$trended.dispersion)[o], col = "steelblue1",lwd = 2)
 legend("topright",c("NB model, NB simul.","NB model, ZINB simul.","ZINB model, ZINB simul."),lty=1,lwd=2,col=c("red","blue","steelblue1"),bty="n")
-mtext("d" ,at=-5, font=2, cex=4/3)
+mtext("c" ,at=-5, font=2, cex=4/3)
 
 
 ### scRNA-seq
-plot(dIslam$AveLogCPM,sqrt(dIslam$tagwise.dispersion),pch=16,cex=.2,col=alpha("black",1/3),xlim=c(1.8,12), xlab="Average Log CPM", ylab="BCV")
+plot(dIslam$AveLogCPM,sqrt(dIslam$tagwise.dispersion),pch=16,cex=.2,col=alpha("black",1/3),xlim=c(1.8,12), xlab="Average log CPM", ylab="BCV", ylim=c(0,15))
 o <- order(dIslam$AveLogCPM)
 lines(dIslam$AveLogCPM[o], sqrt(dIslamTrend$trended.dispersion)[o], col = "red",lwd = 2)
-mtext("e" ,at=-0.5, font=2, cex=4/3)
+mtext("d" ,at=-0.5, font=2, cex=4/3)
 
-par(mar=c(5,5,2,1))
-plot(x=log(colSums(islam)),y=rowMeans(getPi(zinbModel)),pch=19,cex=2/3, xlab="Log library size", ylab=expression(pi[i]*' = '*E[j]*'('*w[ij]*')'))
-mtext("f" ,at=6.7, font=2, cex=4/3)
+plot(x=fprSc,y=tprSc,type="l", xlab="False positive rate", ylab="True positive rate", lwd=2, col="steelblue")
+legend("bottomright", "Islam ZINB-WaVE weights", lty=1, col="steelblue", bty="n")
+mtext("e" ,at=-0.22, font=2, cex=4/3)
 
-hist(zeroWeights[islam==0],main="",xlab="Posterior probability")
-mtext("g" ,at=-.22, font=2, cex=4/3)
 
-plot(dW$AveLogCPM,sqrt(dW$tagwise.dispersion),pch=16,cex=.2,xlab="Average Log CPM", ylab="BCV", xlim=c(1.8,12), ylim=c(0,12),col=alpha("black",1/3))
+plot(dW$AveLogCPM,sqrt(dW$tagwise.dispersion),pch=16,cex=.2,xlab="Average log CPM", ylab="BCV", xlim=c(1.8,12), ylim=c(0,15),col=alpha("black",1/3))
 o=order(dIslamTrend$AveLogCPM)
 lines(dIslamTrend$AveLogCPM[o], sqrt(dIslamTrend$trended.dispersion)[o], col = "red",lwd = 2)
 lines(dW$AveLogCPM[o], sqrt(dW$trended.dispersion)[o], col = "steelblue1",lwd = 2)
 legend("topright",c("NB model, scRNA-seq","ZINB model, scRNA-seq"),lty=1,col=c("red","steelblue1"),lwd=2,bty="n")
-mtext("h" ,at=-0.8, font=2, cex=4/3)
+mtext("f" ,at=-0.8, font=2, cex=4/3)
+
 dev.off()
